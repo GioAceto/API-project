55cc54994cd413ecf0f27c63c0f7fcae46238911
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-04 15:35:48 -0400

    add route to index pug file

--- controllers/cont-data.js
+++ controllers/cont-data.js
@@ -2,6 +2,7 @@ const errorFunction = (req, res) => {
   res.status(404).send('ERROR CODE 404 PAGE NOT FOUND');
 };
 
+
 module.exports = {
   errorFunction
 }
--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const {
   errorFunction
 } = require('./controllers/cont-data')
+
 const PORT = 1400
 const app = express()
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.get('*', errorFunction)
 
