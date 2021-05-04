d42f2d8af24763136baaaddea341433abcef9386
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-03 22:18:48 -0400

    move errorFunction handler into cont-data js file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const error = require('./controllers/cont-data')
+const {
+  errorFunction
+} = require('./controllers/cont-data')
 const PORT = 1400
 const app = express()
 
@@ -7,9 +9,7 @@ app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 
-app.get('*', function (req, res) {
-  res.status(404).send('ERROR CODE 404 PAGE NOT FOUND');
-});
+app.get('*', errorFunction)
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`) // eslint-disable-line no-console
