241ef31ff914ebee2b2bcf7e701b020c03f57be0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-03 22:02:13 -0400

    add route for 404 error

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-
 const PORT = 1400
-
 const app = express()
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
+
+app.all('*'), (request, response) => {
+  return request.sendStatus(404)
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`) // eslint-disable-line no-console
