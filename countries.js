e770093219b1909dea7a4109bcb8551c02ac2b1c
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-05-05 00:10:34 -0400

    create countries js file and ad data for north american countries

---
+++ countries.js
@@ -0,0 +1,31 @@
+const countries = [{
+  name: 'The United States of America',
+  region: 'North America',
+  area: '3.531 Million Sq Miles',
+  population: '328.2',
+  gdp: '21,433,226',
+  export: 'Refined Petroleum',
+  currency: 'US Dollar',
+  leader: 'Joe Biden'
+},
+{
+  name: 'Canada',
+  region: 'North America',
+  area: '3,511,022',
+  population: '37.59',
+  gdp: '1,741,496',
+  export: 'Crude Petroleum',
+  currency: 'Canadian Dollar',
+  leader: 'Justin Trudeau'
+},
+{
+  name: 'Mexico',
+  region: 'North America',
+  area: '750,563',
+  population: '127.6',
+  gdp: '1,256,440',
+  export: 'Cars',
+  currency: 'Mexican Peso',
+  leader: 'Andrés Manuel López Obrador'
+}
+]
