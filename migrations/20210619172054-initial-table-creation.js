5903ca63a77e7c015df542f2594e16954491f4dd
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-19 15:08:09 -0400

    add migration data for table creation

---
+++ migrations/20210619172054-initial-table-creation.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('languages', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      language: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    await queryInterface.createTable('regions', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      region: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    await queryInterface.createTable('countries', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      capital: { type: Sequelize.STRING },
+      area: { type: Sequelize.STRING },
+      population: { type: Sequelize.STRING },
+      gdp: { type: Sequelize.STRING },
+      export: { type: Sequelize.STRING },
+      currency: { type: Sequelize.STRING },
+      leader: { type: Sequelize.STRING },
+      languagesId: { type: Sequelize.INTEGER, references: { model: 'languages', key: 'id' } },
+      flag: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+
+    return queryInterface.createTable('regionsCountries', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      regionsId: { type: Sequelize.INTEGER },
+      countriesId: { type: Sequelize.INTEGER },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE },
+    })
+  },
+
+  down: async (queryInterface) => {
+
+    await queryInterface.dropTable('languages')
+
+    await queryInterface.dropTable('regions')
+
+    await queryInterface.dropTable('countries')
+
+    return queryInterface.dropTable('regionsCountries')
+  }
+}
