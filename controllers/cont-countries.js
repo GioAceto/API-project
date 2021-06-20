3e4ce8bf0e47f3dfcc1a41b847ecae1763258475
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-20 18:35:57 -0400

    add get methods that return data for NA, CA, CR, and SA

--- controllers/cont-countries.js
+++ controllers/cont-countries.js
@@ -22,21 +22,114 @@ const getAllCountries = async (req, res) => {
 
 const getNACountries = async (req, res) => {
   const countries = await models.Countries.findAll({
-    include: [
-      {
-        model: models.Regions
-      }
-    ]
+    include: [{
+      model: models.Regions,
+      where: { region: 'North America' }
+    }]
   })
 
   return res.send(countries)
 }
 
+const getCACountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Central America' }
+    }]
+  })
 
+  return res.send(countries)
+}
+
+const getCRCountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Caribbean' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getSACountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'South America' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getEUCountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Europe' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getMECountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Middle_East' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getAFCountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Africa' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getASCountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Asia' }
+    }]
+  })
+
+  return res.send(countries)
+}
+
+const getOCCountries = async (req, res) => {
+  const countries = await models.Countries.findAll({
+    include: [{
+      model: models.Regions,
+      where: { region: 'Oceania' }
+    }]
+  })
+
+  return res.send(countries)
+}
 
 module.exports = {
   errorFunction,
   getMain,
   getAllCountries,
-  getNACountries
+  getNACountries,
+  getCACountries,
+  getCRCountries,
+  getSACountries,
+  getEUCountries,
+  getMECountries,
+  getAFCountries,
+  getASCountries,
+  getOCCountries
 }
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const countries = require('./countries')
-const { errorFunction, getMain, getAllCountries, getNACountries } = require('./controllers/cont-countries')
+const { errorFunction,
+  getMain,
+  getAllCountries,
+  getNACountries,
+  getCACountries,
+  getCRCountries,
+  getSACountries,
+  getEUCountries,
+  getMECountries,
+  getAFCountries,
+  getASCountries,
+  getOCCountries } = require('./controllers/cont-countries')
 
 const PORT = 1400
 const app = express()
@@ -13,40 +24,15 @@ app.use(express.static('public'))
 app.get('/', getMain)
 
 app.get('/countries/', getAllCountries)
-
 app.get('/countries/north_america', getNACountries)
-
-// app.get('/countries/central_america', (req, res) => {
-//   res.render('central_america', { countries })
-// })
-
-// app.get('/countries/caribbean', (req, res) => {
-//   res.render('caribbean', { countries })
-// })
-
-// app.get('/countries/south_america', (req, res) => {
-//   res.render('south_america', { countries })
-// })
-
-// app.get('/countries/europe', (req, res) => {
-//   res.render('europe', { countries })
-// })
-
-// app.get('/countries/africa', (req, res) => {
-//   res.render('africa', { countries })
-// })
-
-// app.get('/countries/middle_east', (req, res) => {
-//   res.render('middle_east', { countries })
-// })
-
-// app.get('/countries/asia', (req, res) => {
-//   res.render('asia', { countries })
-// })
-
-// app.get('/countries/oceania', (req, res) => {
-//   res.render('oceania', { countries })
-// })
+app.get('/countries/central_america', getCACountries)
+app.get('/countries/caribbean', getCRCountries)
+app.get('/countries/south_america', getSACountries)
+app.get('/countries/europe', getEUCountries)
+app.get('/countries/middle_east', getMECountries)
+app.get('/countries/africa', getAFCountries)
+app.get('/countries/asia', getASCountries)
+app.get('/countries/oceania', getOCCountries)
 
 app.get('*', errorFunction)
 
